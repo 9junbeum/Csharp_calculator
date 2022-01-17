20138e8890a4162fdab782a8d7b7cc04351fac1e
준범 구 <9junbeum@naver.com>
2022-01-17 11:22:34 +0900

    .

--- practice_1_계산기/Form1.Designer.cs
+++ practice_1_계산기/Form1.Designer.cs
@@ -272,7 +272,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(276, 426);
+            this.ClientSize = new System.Drawing.Size(276, 423);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button18);
